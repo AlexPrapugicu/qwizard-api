6f530b8770aae9c99d42902e0b03729d3fff4f1e
Alex Prapugicu <40821161+AlexPrapugicu@users.noreply.github.com>
2021-04-14 11:46:03 +0300

    updated model file for quiz with necessary relationships and validations

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,11 @@
 
 class Quiz < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: {minimum: 2, maximum: 50}
+
+  has_many :questions, dependent: :destroy
+  has_many :lobbies, dependent: :destroy
+
+  validates_associated :questions
+  validates_associated :lobbies
 end
